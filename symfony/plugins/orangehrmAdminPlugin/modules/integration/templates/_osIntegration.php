628fbf2dca4aa7cd5db772c3b50a001d9ec79411
shamithasilva <shamithadevinda@gmail.com>
2018-01-04 11:18:06 +0530

    Button change

--- symfony/plugins/orangehrmAdminPlugin/modules/integration/templates/_osIntegration.php
+++ symfony/plugins/orangehrmAdminPlugin/modules/integration/templates/_osIntegration.php
@@ -144,7 +144,6 @@
 
 <div>
     <div class ='toggl'>
-        <button type="button" id="sycToggl" class="syncToggl" onclick="syncToggl()">Sync With Toggl</button>
     <?php echo html_entity_decode($page['html']); ?>
         <div class="loader loader--style2" title="1">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="loader-1" class="svgcl" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
